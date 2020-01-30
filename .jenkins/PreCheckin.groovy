2f70abba369ab8bb34e4b730b01f62634549c51d
Eiden Yoshida <eiden.yoshida@amd.com>
2020-01-30 15:44:16 -0700

    Change compile command parameter

--- .jenkins/PreCheckin.groovy
+++ .jenkins/PreCheckin.groovy
@@ -28,7 +28,7 @@ rocFFTCI:
         platform, project->
 
         commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/Common.groovy"
-        commonGroovy.runCompileCommand(platform, project, true)
+        commonGroovy.runCompileCommand(platform, project, false, true)
     }
 
     def testCommand =
